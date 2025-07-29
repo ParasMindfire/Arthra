910f649a80ac41f00bfee7eabaf328d0ca5ec7ec
ParasMindfire <mfsi.parasb@outlook.com>
2025-07-29 14:52:02 +0530

    feat : initialise

---
+++ src/main/java/com/paras/Arthra/ArthraApplication.java
@@ -0,0 +1,13 @@
+package com.paras.Arthra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paras/Arthra/ArthraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paras.Arthra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
