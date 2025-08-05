d4e0ea44fe9224d3e1fca17340596d868f976133
ParasMindfire <mfsi.parasb@outlook.com>
2025-08-05 12:22:50 +0530

    feat : add prod credentials for db

--- src/main/java/com/paras/Arthra/service/ProfileService.java
+++ src/main/java/com/paras/Arthra/service/ProfileService.java
@@ -3,6 +3,7 @@ package com.paras.Arthra.service;
 import java.util.Map;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -29,12 +30,15 @@ public class ProfileService {
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
 
+    @Value("${app.activation.url}")
+    private String activationUrl;
+
     public ProfileDTO registerProfile(ProfileDTO profileDTO){
         ProfileEntity newProfile=toEntity(profileDTO);
         newProfile.setActivationToken(UUID.randomUUID().toString());
         newProfile=profileRepository.save(newProfile);
 
-        String activationLink="http://localhost:8080/api/v1.0/activate?token="+newProfile.getActivationToken();
+        String activationLink=activationUrl+"/api/v1.0/activate?token="+newProfile.getActivationToken();
         String subject="Activate your Money Manager Account ";
         String body="Click on the following link to activate your account: "+activationLink;
 
